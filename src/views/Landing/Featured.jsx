89991ad1563581a1da4c3e4c68588916f045070d
Alexandre Betioli <apbetioli@gmail.com>
2020-08-19 14:48:46 -0300

    Use progressive jpgs

--- src/views/Landing/Featured.jsx
+++ src/views/Landing/Featured.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 class Featured extends React.Component {
   render() {
     const asset = "/" + this.props.asset;
-    const background = require("assets/img" + asset + ".png");
+    const background = require("assets/img" + asset + ".jpg");
     return (
       <Link to={asset}>
         <article className="hero-case animate fadeInUp one">
